9b079c3b9effa2cde6211b50131dccbbc4ed91db
Juan Miguel  Arias Mejias <juanmiguelar09@gmail.com>
2025-09-01 17:49:46 -0600

    refactor: make site url configurable

--- server/routes/sitemap.xml.ts
+++ server/routes/sitemap.xml.ts
@@ -1,9 +1,12 @@
 import { defineEventHandler, setHeader } from 'h3'
+import { useRuntimeConfig } from '#imports'
 
 /**
  * Basic dynamic sitemap generation for portfolio pages.
  */
 export default defineEventHandler((event) => {
+  const { public: { siteUrl } } = useRuntimeConfig()
+
   const urls = [
     '/',
     '/projects',
@@ -13,7 +16,7 @@ export default defineEventHandler((event) => {
 
   const body = `<?xml version="1.0" encoding="UTF-8"?>\n` +
     `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n` +
-    urls.map(u => `  <url><loc>https://juanmiguel.dev${u}</loc></url>`).join('\n') +
+    urls.map(u => `  <url><loc>${siteUrl}${u}</loc></url>`).join('\n') +
     `\n</urlset>`
 
   setHeader(event, 'content-type', 'application/xml')
