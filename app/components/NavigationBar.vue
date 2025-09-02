08c038ed7eb0c63df9aa169d9ef688c8d578db89
iamjuandev <juanmiguelar09@gmail.com>
2025-09-01 22:27:37 -0600

    navigation bar

--- app/components/NavigationBar.vue
+++ app/components/NavigationBar.vue
@@ -7,13 +7,18 @@
       <v-btn v-for="link in links" :key="link.href" :href="link.href" variant="text" class="mx-1" aria-label="Go to section">{{ link.label }}</v-btn>
     </div>
   </v-app-bar>
-  <v-navigation-drawer v-model="drawer" temporary class="d-md-none">
-    <v-list>
-      <v-list-item v-for="link in links" :key="link.href" :href="link.href" @click="drawer = false" :title="link.label" />
-      <v-list-item @click="toggleTheme" title="Toggle theme" prepend-icon="mdi-theme-light-dark" />
-    </v-list>
-  </v-navigation-drawer>
+  
+  <!-- Wrap the drawer in ClientOnly -->
+  <ClientOnly>
+    <v-navigation-drawer v-model="drawer" temporary class="d-md-none">
+      <v-list>
+        <v-list-item v-for="link in links" :key="link.href" :href="link.href" @click="drawer = false" :title="link.label" />
+        <v-list-item @click="toggleTheme" title="Toggle theme" prepend-icon="mdi-theme-light-dark" />
+      </v-list>
+    </v-navigation-drawer>
+  </ClientOnly>
 </template>
+
 <script setup lang="ts">
 import { ref } from 'vue'
 import { useTheme } from 'vuetify'
