5c97e3aa611d3ee9941561a201032f98f5d5efb5
iamjuandev <juanmiguelar09@gmail.com>
2025-09-01 22:22:40 -0600

    updating seohead

--- app/components/global/SeoHead.vue
+++ app/components/global/SeoHead.vue
@@ -1,11 +1,12 @@
+<!-- components/SeoHead.vue -->
 <script setup lang="ts">
 import { useSeo } from '~/composables/useSeo'
 import type { SeoMetaOptions } from '~/types/seo'
 
 const props = defineProps<SeoMetaOptions>()
-
-// Apply SEO meta information using the central composable
 useSeo().setSeo(props)
 </script>
 
-<template></template>
+<template>
+  <!-- SEO component -->
+</template>
