6dcca595c8bed99e34a1089f15d304f4705f65cc
iamjuandev <juanmiguelar09@gmail.com>
2025-09-01 23:58:39 -0600

    fixing hidratation

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,9 +1,4 @@
 <template>
-  <SeoHead
-    title="Juan Miguel - Full-Stack Developer Portfolio"
-    description="Experienced developer specializing in Vue, Nuxt, and Golang"
-    :schema="schema"
-  />
   <HeroSection />
   <ServicesSection />
   <ProjectsSection />
@@ -13,7 +8,6 @@
   <ContactForm />
 </template>
 <script setup lang="ts">
-import SeoHead from '~/components/global/SeoHead.vue'
 import HeroSection from '~/components/HeroSection.vue'
 import ServicesSection from '~/components/ServicesSection.vue'
 import ProjectsSection from '~/components/ProjectsSection.vue'
@@ -21,11 +15,4 @@ import MetricsSection from '~/components/MetricsSection.vue'
 import RecomendationsSection from '~/components/RecomendationsSection.vue'
 import TechStackSection from '~/components/TechStackSection.vue'
 import ContactForm from '~/components/ContactForm.vue'
-
-const schema = {
-  '@context': 'https://schema.org',
-  '@type': ['Person', 'WebSite'],
-  name: 'Juan Miguel',
-  url: 'https://juanmiguel.dev'
-}
 </script>
